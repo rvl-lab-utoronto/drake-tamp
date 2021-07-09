13a9a76d6da8789c83445199ceeac989fbdd57b8
Mohamed Khodeir <m.khodeir@mail.utoronto.ca>
2021-07-09 18:24:16 +0000

    object gnn + fact model + graphnetwork working

--- learning/gnn/data.py
+++ learning/gnn/data.py
@@ -1,3 +1,5 @@
+#%%
+import numpy as np
 from learning.oracle import objects_from_facts
 import itertools
 import torch
@@ -16,6 +18,14 @@ def construct_fact_graph(goal_facts, atom_map, stream_map):
     for i, fact in enumerate(atom_map):
         nodes.append(fact)
         fact_to_idx[fact] = i
+        edges.append((i, i))
+        edge_attributes_list.append({
+            "is_directed": False,
+            "stream": None,
+            "domain_index": -1,
+            "via_objects": []
+        })
+
     for fact, i in fact_to_idx.items():
         fact_objects = objects_from_facts([fact])
         node_attributes = {
@@ -37,12 +47,12 @@ def construct_fact_graph(goal_facts, atom_map, stream_map):
                     "is_directed": True,
                     "stream": stream_map[fact],
                     "via_objects": objects_from_facts([parent]).intersection(fact_objects),
-                    "domain_index": domain_idx # meant to encode the position in which 
+                    "domain_index": domain_idx # meant to encode the position in which
                 }
                 edge_attributes_list.append(edge_attributes)
-    
+
     # add object sharing edges in initial objects
-    object_edges = {} 
+    object_edges = {}
     for o in initial_objects:
         fact_idxs = initial_objects[o]
         for i, j in itertools.combinations(fact_idxs, 2):
@@ -72,7 +82,7 @@ class ModelInfo:
     @property
     def node_feature_size(self):
         return len(self.predicate_to_index) + 2
-    
+
     @property
     def edge_feature_size(self):
         return len(self.stream_to_index) + 3
@@ -83,6 +93,12 @@ class ModelInfo:
     @property
     def predicate_to_index(self):
         return {s:i for i,s in enumerate(self.predicates)}
+
+    @property
+    def object_node_feature_size(self):
+        return len(self.predicates)
+
+
 
 
 def construct_input(parents, candidate_stream, atom_map, stream_map, model_info):
@@ -105,35 +121,66 @@ def construct_input(parents, candidate_stream, atom_map, stream_map, model_info)
         edge_features[i, -2] = int(attr['is_directed'])
         edge_features[i, -1] = len(attr['via_objects'])
 
+    objects_data = get_object_graph(atom_map, model_info)
+
+    nodes_ind = []
+    for n in nodes:
+        predicate = n[0]
+        objects = n[1:]
+        nodes_ind.append([model_info.predicate_to_index[predicate]] + [objects_data.object_to_index[o] for o in objects])
+
     return Data(
         nodes=nodes,
         x=node_features,
         edge_attr=edge_features,
         edge_index=edge_index,
-        candidate=candidate
+        candidate=candidate,
+        objects_data=objects_data,
+        nodes_ind=nodes_ind
     )
+
+def get_object_predicates(object_name, atom_map):
+    return set({a[0] for a in atom_map if object_name in a})
+
+def get_object_graph(atom_map, model_info):
+    objects = list(objects_from_facts([a for a in atom_map]))
+    object_to_index = {o:i for i, o in enumerate(objects)}
+
+    predicate_hot = np.zeros((len(objects), len(model_info.predicates)))
+    for i, o in enumerate(objects):
+        for predicate in get_object_predicates(o, atom_map):
+            predicate_hot[i, model_info.predicate_to_index[predicate]] = 1
+    embs = predicate_hot
+    edges = set()
+    for fact in atom_map:
+        for source in objects:
+            edges.add((object_to_index[source], object_to_index[source]))
+        for source, dest in itertools.combinations(objects, 2):
+            if source in fact and dest in fact:
+                edges.add((object_to_index[source], object_to_index[dest]))
+                edges.add((object_to_index[dest], object_to_index[source]))
+    return Data(object_to_index=object_to_index,x=torch.tensor(embs, dtype=torch.float), edge_index=torch.tensor(list(edges), dtype=torch.long).t().contiguous())
 
 
 def parse_labels(pkl_path):
     with open(pkl_path, 'rb') as f:
         data = pickle.load(f)
-    
+
     model_info = ModelInfo(
         goal_facts=data['goal_facts'],
         predicates=[p.name for p in data['domain'].predicates],
         streams=[None] + [e['name'] for e in data['externals']],
-        stream_input_sizes=[None] + [len(e['domain']) for e in data['externals']]
+        stream_input_sizes=[None] + [len(e['domain']) for e in data['externals']],
     )
 
     dataset = []
     for label in data['labels']:
-        fact, parents, candidate_stream, is_relevant, atom_map, stream_map = label
+        _, parents, candidate_stream, is_relevant, atom_map, stream_map = label
         d = construct_input(parents, candidate_stream, atom_map, stream_map, model_info)
         d.y = torch.tensor([float(is_relevant)])
-        d.fact = fact
         dataset.append(d)
-
     return dataset, model_info
+
 
 if __name__ == '__main__':
     dataset = parse_labels('/home/mohammed/drake-tamp/learning/data/labeled/2021-07-05-14:35:28.341.pkl')
--- learning/gnn/models.py
+++ learning/gnn/models.py
@@ -4,32 +4,45 @@ import torch.nn.functional as F
 from torch_scatter import scatter_mean
 from torch_geometric.nn import MetaLayer
 from torch_geometric.nn import GCNConv
+from torch.nn.utils.rnn import pad_sequence
 
 class StreamInstanceClassifier(nn.Module):
-    def __init__(self, node_feature_size, edge_feature_size, stream_input_sizes, feature_size=8, mlp_out=1, use_gcn=True):
+    def __init__(self, node_feature_size, edge_feature_size, stream_input_sizes, num_predicates, object_node_feature_size, feature_size=8, lstm_size=10,  mlp_out=1, use_gcn=True, use_object_model=True):
         super(StreamInstanceClassifier, self).__init__()
-        if use_gcn:
+        self.use_object_model = use_object_model
+        self.use_gcn = use_gcn
+        if self.use_object_model:
+            self.fact_model = FactModel(num_predicates, input_size=lstm_size, hidden_size=lstm_size)
+            self.object_network = SimpleGCN(object_node_feature_size, lstm_size)
+            node_feature_size += lstm_size
+
+        if self.use_gcn:
             self.graph_network = SimpleGCN(node_feature_size, feature_size)
         else:
-            raise NotImplementedError("There is something wrong with this implementation of meta layers.")
             self.graph_network = GraphNetwork(
                 node_feature_size=node_feature_size,
                 edge_feature_size=edge_feature_size,
                 hidden_size=feature_size,
             )
-        self.mlps = tuple([MLP([feature_size, mlp_out], feature_size * s) for s in stream_input_sizes])
+        self.mlps = tuple([MLP([16, mlp_out], feature_size*s)  for s in stream_input_sizes])
         for i, mlp in enumerate(self.mlps):
             setattr(self, f'mlp{i}', mlp)
 
 
     def forward(self, data, score=False):
-        x = self.graph_network(data)
-        # assert len(data.candidate) == 1, "Cant support batching yet"
+        if self.use_object_model:
+            object_representations = self.object_network(data.objects_data)
+            fact_representations = self.fact_model(data, object_representations)
+            data.z = torch.cat((data.x, fact_representations), dim=1)
+            x = self.graph_network(data, attr='z')
+        else:
+            x = self.graph_network(data, attr='x')
+
         cand = data.candidate
         assert cand[0] > 0, "Considering an initial condition?"
-        mlp = self.mlps[cand[0] - 1]
-        parents = torch.cat([x[p] for p in  cand[1:]])
-        x = mlp(parents)
+        stream_mlp = self.mlps[cand[0] - 1]
+        domain_facts = torch.cat([x[p] for p in  cand[1:]])
+        x = stream_mlp(domain_facts)
         if score:
             return torch.sigmoid(x)
         return x
@@ -39,12 +52,12 @@ class EdgeModel(nn.Module):
         super(EdgeModel, self).__init__()
         self.edge_mlp = nn.Sequential(
             nn.Linear(2 * node_feature_size + edge_feature_size, hidden_size),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.LayerNorm(hidden_size),
             nn.Dropout(dropout) if dropout > 0.0 else nn.Identity(),
             nn.Linear(hidden_size, hidden_size),
         )
-    
+
     def forward(self, src, dst, edge_attr, u=None, batch=None):
         # src, dst: [E, F_x], where E is num edges, F_x is node-feature dimensionality
         # edge_attr: [E, F_e], where E is num edges, F_e is edge-feature dimensionality
@@ -57,24 +70,24 @@ class NodeModel(nn.Module):
         super(NodeModel, self).__init__()
         self.node_mlp_1 = nn.Sequential(
             nn.Linear(node_feature_size + hidden_size, hidden_size),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.LayerNorm(hidden_size),
             nn.Dropout(dropout) if dropout > 0.0 else nn.Identity(),
             nn.Linear(hidden_size, hidden_size),
         )
         self.node_mlp_2 = nn.Sequential(
             nn.Linear(node_feature_size + hidden_size, hidden_size),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.LayerNorm(hidden_size),
             nn.Dropout(dropout) if dropout > 0.0 else nn.Identity(),
             nn.Linear(hidden_size, n_targets),
         )
-    
+
     def forward(self, x, edge_idx, edge_attr, u=None, batch=None):
         row, col = edge_idx
-        out = torch.cat([x[col], edge_attr], dim=1)
+        out = torch.cat([x[row], edge_attr], dim=1)
         out = self.node_mlp_1(out)
-        out = scatter_mean(out, row, dim=0, dim_size=x.size(0))
+        out = scatter_mean(out, col, dim=0, dim_size=x.size(0))
         out = torch.cat([x, out], dim=1)
         return self.node_mlp_2(out)
 
@@ -91,9 +104,9 @@ class GraphNetwork(nn.Module):
             edge_model=EdgeModel(node_feature_size, edge_feature_size, hidden_size, dropout=dropout),
             node_model=NodeModel(node_feature_size, hidden_size, n_targets, dropout=dropout),
         )
-    
-    def forward(self, data):
-        x, edge_idx, edge_attr = data.x, data.edge_index, data.edge_attr
+
+    def forward(self, data, attr='x'):
+        x, edge_idx, edge_attr = getattr(data, attr), data.edge_index, data.edge_attr
         x, edge_attr, _ = self.meta_layer_1(x, edge_idx, edge_attr)
         x, edge_attr, _ = self.meta_layer_2(x, edge_idx, edge_attr)
         x, edge_attr, _ = self.meta_layer_3(x, edge_idx, edge_attr)
@@ -105,14 +118,14 @@ class SimpleGCN(nn.Module):
         self.conv1 = GCNConv(node_feature_size, feature_size)
         self.conv2 = GCNConv(feature_size, feature_size)
         self.conv3 = GCNConv(feature_size, feature_size)
-    def forward(self, data):
-        x, edge_index = data.x, data.edge_index
+    def forward(self, data, attr = 'x'):
+        x, edge_index = getattr(data, attr), data.edge_index
 
         x = self.conv1(x, edge_index)
-        x = F.relu(x)
+        x = F.leaky_relu(x)
         x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index)
-        x = F.relu(x)
+        x = F.leaky_relu(x)
         x = F.dropout(x, training=self.training)
         x = self.conv3(x, edge_index)
         return x
@@ -121,7 +134,7 @@ def MLP(layers, input_dim, dropout=0.0):
     mlp_layers = [torch.nn.Linear(input_dim, layers[0])]
 
     for layer_num in range(0, len(layers) - 1):
-        mlp_layers.append(torch.nn.ReLU())
+        mlp_layers.append(torch.nn.LeakyReLU())
         mlp_layers.append(torch.nn.Linear(layers[layer_num], layers[layer_num + 1]))
     # if len(layers) > 1:
     #     mlp_layers.append(torch.nn.LayerNorm(mlp_layers[-1].weight.size()[:-1]))
@@ -129,16 +142,25 @@ def MLP(layers, input_dim, dropout=0.0):
     #         mlp_layers.append(torch.nn.Dropout(p=dropout))
     return torch.nn.Sequential(*mlp_layers)
 
-class FactModel(nn.Module):
-    def __init__(self, predicates, object_names, input_size=4, hidden_size=4):
-        self.to_index = {k:i for i, k in enumerate(predicates + object_names)}
-        self.unknown = len(self.to_index)
-        self.embedding = nn.Embedding(1 + len(predicates) +  len(object_names), input_size)
-        self.lstm = nn.LSTM(input_size, hidden_size=hidden_size, num_layers=1)
-    def forward(self, data):
 
-        data = torch.tensor([[self.to_index.get(arg, self.unknown) for arg in fact] for fact in data.nodes])
-        x = self.embedding(data)
+class FactModel(nn.Module):
+    def __init__(self, num_predicates, input_size=4, hidden_size=4):
+        super(FactModel, self).__init__()
+        self.predicate_embedding = nn.Embedding(num_predicates, input_size)
+        self.lstm = nn.LSTM(input_size, hidden_size=hidden_size, num_layers=1)
+    def forward(self, data, object_embeddings):
+        preds = self.predicate_embedding(torch.tensor([n[:1] for n in data.nodes_ind]))
+        fact_tokens = []
+        for i in range(len(data.nodes_ind)):
+            n = data.nodes_ind[i]
+            if len(n) > 1:
+                objs = [object_embeddings[i] for i in n[1:]]
+                fact_tokens.append(torch.stack([preds[i].squeeze(0)] + objs))
+            else:
+                fact_tokens.append(preds[i])
+
+        x = pad_sequence(fact_tokens)
+
         x, _ = self.lstm(x)
         x = x[-1] # select last output
         return x
